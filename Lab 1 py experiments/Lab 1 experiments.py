a17f25f6a874d1624388e829f77b1aa51c16cba1
Lars <Lars.Git.Johansson@outlook.com>
2021-05-13 01:56:13 +0200

    first commit

---
+++ Lab 1 py experiments/Lab 1 experiments.py
@@ -0,0 +1,175 @@
+# Download Data
+
+import urllib.request
+
+print('Beginning file download...')
+
+url = 'https://s3-us-west-1.amazonaws.com/udacity-selfdrivingcar/traffic-signs-data.zip'
+
+urllib.request.urlretrieve(url, './traffic-signs-data.zip')
+
+# Unzip File
+
+import zipfile
+import os
+
+print('Beginning file unzip')
+
+zip_ref = zipfile.ZipFile('./traffic-signs-data.zip', 'r')
+zip_ref.extractall('./')
+zip_ref.close()
+
+print('Done')
+os.listdir("./")
+
+# Step 0: Load The Data
+
+# Load pickled data
+import pickle
+
+# TODO: Fill this in based on where you saved the training and testing data
+
+training_file = './train.p'
+validation_file= './valid.p'
+testing_file = './test.p'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+    
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+import numpy as np
+# import pandas as pd
+
+### Replace each question mark with the appropriate value. 
+### Use python, pandas or numpy methods rather than hard coding the results
+
+# TODO: Number of training examples
+n_train = X_train.shape[0]
+
+# TODO: Number of validation examples
+n_validation = X_valid.shape[0]
+
+# TODO: Number of testing examples.
+n_test = X_test.shape[0]
+
+# TODO: What's the shape of an traffic sign image?
+image_shape = X_train.shape[1:]
+
+# TODO: How many unique classes/labels there are in the dataset.
+n_classes = len(np.unique(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of testing examples =", n_test)
+print("Number of validation examples =", n_validation)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+def show_images(images, cols = 1, titles = None):
+    """Display a list of images in a single figure with matplotlib.
+    
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    
+    n_images = len(images)
+    
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    
+    fig = plt.figure(figsize=(2, 2))
+    
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        a.grid(False)
+        a.axis('off')
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image, cmap='gray')
+        a.set_title(title)
+    
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
+    
+def select_random_images_by_classes(features, labels, n_features):
+  
+  indexes = []
+  _classes = np.unique(labels);
+  
+  while len(indexes) < len(_classes):
+  
+    index = random.randint(0, n_features-1)
+    _class = labels[index]
+
+    for i in range(0, len(_classes)):
+
+      if _class == _classes[i]:
+        _classes[i] = -1
+        indexes.append(index)
+        break
+
+  images = []
+  titles = []
+
+  for i in range(0, len(indexes)):
+    images.append(features[indexes[i]])
+    titles.append("class " + str(labels[indexes[i]]))
+
+  show_images(images, titles = titles)
+
+### Data exploration visualization code goes here.
+### Feel free to use as many code cells as needed.
+import matplotlib.pyplot as plt
+import random
+# Visualizations will be shown in the notebook.
+%matplotlib inline
+
+select_random_images_by_classes(X_train, y_train, n_train)
+
+def plot_distribution_chart(x, y, xlabel, ylabel, width, color):
+  
+  plt.figure(figsize=(15,7))
+  plt.ylabel(ylabel, fontsize=18)
+  plt.xlabel(xlabel, fontsize=16)
+  plt.bar(x, y, width, color=color)
+  plt.show()
+
+_classes, counts = np.unique(y_train, return_counts=True)
+
+plot_distribution_chart(_classes, counts, 'Classes', '# Training Examples', 0.7, 'blue')
+
+# 3. Convert images to grayscale ---------------------------------
+
+X_train_gray = np.sum(X_train/3, axis=3, keepdims=True)
+
+X_test_gray = np.sum(X_test/3, axis=3, keepdims=True)
+
+X_valid_gray = np.sum(X_valid/3, axis=3, keepdims=True)
+
+# check grayscale images
+select_random_images_by_classes(X_train_gray.squeeze(), y_train, n_train)
+
+# 4. Mean Substraction
+
+X_train_gray -= np.mean(X_train_gray)
+
+X_test_gray -= np.mean(X_test_gray)
+
+X_train = X_train_gray
+
+X_test = X_test_gray
+
+select_random_images_by_classes(X_train_gray.squeeze(), y_train, n_train)
